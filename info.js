d7ff797f233495cd66f26a3867bcaa034867de51
WYXkk <2068559520@qq.com>
2024-08-18 23:21:34 +0800

    update

--- info.js
+++ info.js
@@ -1,107 +1,165 @@
 const modList=[
 	{
 		"category":"Turbo mods",
-		"description":"These mods only change how the game balances without adding any new content.",
+		"description":`
+			These mods only change how the game balances without adding any new content.
+		`,
 		"list":[
 			{
 				"name":"Antimatter FAST Dimensions",
 				"link":"https://wyxkk.github.io/ADfaster/",
 				"author":"WYXkk",
-				"description":"This mod speed up the game by a constant factor and add various buffs.<br>\
-					Currently it is mostly finished."
+				"description":`
+					This mod speed up the game by a constant factor and add various buffs.<br>
+					Currently it is mostly finished.
+				`
 			},
 			{
 				"name":"Antimatter Dimensions Simplified",
 				"link":"https://arbiterfox.github.io/ADSimplified/",
 				"author":"ArbiterFox",
-				"description":"This mod adds various buffs and changed some mechanics to make the game simple.<br>\
-					Currently it is very buggy and almost abandoned. Also doom is not fully implemented."
+				"description":`
+					This mod adds various buffs and changed some mechanics to make the game simple.<br>
+					Currently it is very buggy and abandoned. Also doom is not fully implemented.
+				`
 			},
 			{
 				"name":"Antimatter FASTER Dimensions",
 				"link":"https://realjman.github.io/ADfasterer/",
 				"author":"jman",
-				"description":"This mod is a mod of the FAST mod, it adds more buffs to make it even faster.<br>\
-					You can easily complete the game in few hours, and sub-1h record already exists."
+				"description":`
+					This mod is a mod of the FAST mod, it adds more buffs to make it even faster.<br>
+					You can easily complete the game in few hours, and sub-1h record already exists.
+				`
+			},
+			{
+				"name":"AD: Resimplified",
+				"link":"https://donaldino7712.github.io/ADResimplified/",
+				"author":"Donaldino7712",
+				"description":`
+					Due to Simplified is abandoned, the author decides to make this mod of the same concept.<br>
+					Currently it is finished.
+				`
+			},
+			{
+				"name":"AD: Smooth",
+				"link":"https://googolplexo.github.io/AntimatterDimensionsSmooth/",
+				"author":"Googolplex",
+				"description":`
+					This mod makes all formula smooth, that is, softcaps are either removed or start at 0.<br>
+					Some mechanics are changed and all contents are rebalanced.<br>
+					Currently it is finished up to pre-eternity (1e2000 IP).
+				`
 			},
 		]
 	},
 	{
 		"category":"Pre-END content mods",
-		"description":"These mods add new content in normal playthrough.",
+		"description":`
+			These mods add new content in normal playthrough.
+		`,
 		"list":[
 			{
 				"name":"AD Unoriginal",
 				"link":"https://hexavault.github.io/AntimatterDimensions-Unoriginal/",
 				"author":"Hexa",
-				"description":"This mod adds the 9th dimension, and a new achievement each row.<br>\
-					There are still some bugs in this mod, but mostly playable."
+				"description":`
+					This mod adds the 9th dimension, and a new achievement each row.<br>
+					There are still some bugs in this mod, but mostly playable.
+				`
 			},
 			{
 				"name":"AD: Rewarded",
 				"link":"https://rangaz.github.io/AntimatterDimensionsWithAchievementRewards/",
 				"author":"Rangazz",
-				"description":"This mod adds a reward for all achievements, and many other contents about achievements.<br>\
-					Currently the whole game is finished."
+				"description":`
+					This mod adds a reward for all achievements, and many other contents about achievements.<br>
+					Currently the whole game is finished.
+				`
 			},
 			{
 				"name":"Antimatter Dimensions Refined",
 				"link":"https://katze08.github.io/Antimatter-Dimensions-Refined/",
 				"author":"Katze08",
-				"description":"This mod is a NG-, by nerfing the game a little bit and add more content to re-accelerate.<br>\
-					Currently it has content up to galaxy stage and some other things implemented, but seemingly abandoned.<br>\
-					(It has some post-END features but very unimplemented)"
+				"description":`
+					This mod is a NG-, by nerfing the game a little bit and add more content to re-accelerate.<br>
+					Currently it has content up to galaxy stage and some other things implemented, but seemingly abandoned.<br>
+					(It has some post-END features but very unimplemented)
+				`
 			},
 			{
 				"name":"Antimatter Dimensions Roguelike",
 				"link":"https://spotky1004.com/AntimatterDimensionsRoguelike/",
 				"author":"spotky1004",
-				"description":"This mod adds roguelike content to the game, starting from first dimension boost.<br>\
-					Currently it has content up to first few infinities, things past that are not implemented."
+				"description":`
+					This mod adds roguelike content to the game, starting from first dimension boost.<br>
+					Currently it has content up to first few infinities, but seemingly abandoned.
+				`
 			},
 			{
 				"name":"Antimatter Dimensions V",
 				"link":"https://glitchyfishys.github.io/AntimatterDimensions-V/",
 				"author":"Glitchyfishy",
-				"description":"This mod adds a new goal system to help you progress, and some other new contents.<br>\
-					Currently it is finished up to cel6."
+				"description":`
+					This mod adds a new goal system to help you progress, and some other new contents.<br>
+					Currently it is finished up to pre-doom.
+				`
 			},
 			{
 				"name":"ADPuzzle",
 				"link":"https://aquamarine309.github.io/ADPuzzleTestVer/",
 				"author":"aquamarine",
-				"description":"This mod focus on creating puzzling gameplay everywhere like cel3 in base game.<br>\
-					Currently it is finished up to pre-eternity."
+				"description":`
+					This mod focus on creating puzzling gameplay everywhere like cel3 in base game.<br>
+					Currently it is finished up to pre-eternity. (There is an eternity beta test)
+				`
+			},
+			{
+				"name":"AD Space Mod",
+				"link":"https://qwqe308.github.io/AD-Space-Mod/",
+				"author":"QwQe308",
+				"description":`
+					This mod adds a new system called \"Space\", nerfing your antimatter but providing space researches.<br>
+					Currently it is finished up to pre-eternity.
+				`
 			},
 		]
 	},
 	{
 		"category":"Post-END content mods",
-		"description":"These mods add new content after the end in the original game.",
+		"description":`
+			These mods add new content after the end in the original game.
+		`,
 		"list":[
 			{
 				"name":"Antimatter Dimensions: Redemption Legacy",
 				"link":"https://toilet45.github.io/ADRedemption/",
 				"author":"Royal (and many other developers)",
-				"description":"This mod adds a new prestige layer \"Mend\" after the original ending.<br>\
-					Besides some simple upgrades and milestones, there are more content like warp reality.<br>\
-					Currently it is abandoned for waiting for Break Eternity port of the game."
+				"description":`
+					This mod adds a new prestige layer \"Mend\" after the original ending.<br>
+					Besides some simple upgrades and milestones, there are more content like warp reality.<br>
+					Currently it is abandoned for waiting for Break Eternity port of the game.
+				`
 			},
 		]
 	},
 	{
-		"category":"AD-code-based different game",
-		"description":"These games use code from AD with different content, and so are <i>*technically*</i> AD mods.",
+		"category":"Different game based on the game's code",
+		"description":`
+			These games use code from AD with different content, and so are <i>*technically*</i> AD mods.
+		`,
 		"list":[
 			{
 				"name":"Jiaozi Increment",
 				"link":"https://aquamarine309.github.io/JiaoziIncrement/",
 				"author":"aquamarine",
-				"description":"This is a game of making jiaozi (or Chinese dumpling). And its default language is Chinese.<br>\
-					You can go to last tab's second subtab then click the topleft button to change language to English.<br>\
-					Not everything have an English translation currently."
+				"description":`
+					This is a game of making jiaozi (or Chinese dumpling). And its default language is Chinese.<br>
+					You can go to last tab's second subtab then click the topleft button to change language to English.<br>
+					Not everything have an English translation currently.
+				`
 			},
 		]
 	},
 ];
-const updateTimeStamp=1720666800;
+const updateTimeStamp=1723996800;
